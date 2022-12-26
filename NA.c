b914b00bdce0cd31718f75813b6d767d368c56ee
satyaaps <satyaariya71@gmail.com>
2022-12-26 19:50:23 +0800

    update for staging

--- NA.c
+++ NA.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define batas_pembelian 120
-#define data_pembelian 115
-#define MAX_LENGTH 100
-//#define MAX_PURCHASE 120
+//#define c.maxpurchase 120
+//#define c.purcase 115
+#define MAX_LENGTH 10
 
 float validasi();
 void jenis_fasilitas();
@@ -39,28 +38,27 @@ void keluar(){
 }
 
 // struktur data untuk menyimpan data pembeli
-typedef struct {
+struct Pembeli{
 	char plate[MAX_LENGTH]; // nomor plat
 	int purchase; // jumlah pembelian
 	int max_purchase;
 } Customer;
 
-int membuka_file(){
+int openFile(struct Pembeli Customer[]){
 	FILE *fp = fopen("customers.txt", "r"); 	// buka file teks
 
 	if (fp == NULL) { // cek apakah file dapat dibuka atau tidak
-	printf("File Tidak Ada\n");
-	return 1;
-
+		printf("File Tidak Ada\n");
+		return 1;
 	}Customer c; // deklarasi variabel c sebagai tipe data Customer
 
-	// baca data dari file teks sampai akhir file
-	while (fscanf(fp, "%s %d", c.plate, &c.purchase) != EOF) {
+	while (fscanf(fp, "%s %d %d", &c.plate, &c.purchase, &c.max_purchase) != EOF) { // baca data dari file teks sampai akhir file
 		if (c.purchase > c.max_purchase) { // cek apakah pembelian melebihi batas maksimum
 		printf("Pembelian melebihi batas maksimum!\n");
 		} else {
 		printf("Nomor plat: %s\n", c.plate);
-		printf("Jumlah pembelian: %d liter\n", c.purchase);
+		printf("Anda sudah membeli sebanyak : %d liter\n", c.purchase);
+		printf("Batas pembelian anda untuk bulan ini : %d liter \n", c.max_purchase);
 		}
 	}
 	fclose(fp);
@@ -69,16 +67,40 @@ int membuka_file(){
 
 
 
-float validasi(){
+void readPlate(char *plate) {
+	int c; // variabel untuk menyimpan karakter input
+	int i = 0; // indeks untuk menyimpan karakter ke dalam string
+
+	// baca input sampai enter atau sampai maksimum panjang string
+	while ((c = getchar()) != '\n' && c != EOF && i < MAX_LENGTH - 1) {
+		plate[i++] = c; // simpan karakter ke dalam string
+	}
+	plate[i] = '\0'; // tambahkan null character di akhir string
+}
+
+// fungsi untuk membaca input jumlah pembelian
+int readPurchase() {
+	int purchase; // variabel untuk menyimpan jumlah pembelian
+
+	// baca input sampai mendapatkan angka yang valid
+	while (scanf("%d", &purchase) != 1) {
+		printf("Input tidak valid, masukkan angka: ");
+	while (getchar() != '\n'); // baca sampai newline
+}
+
+return purchase;
+}
+
+float validasi(int purchase, int max_purchase){
 	float inputan;
 	float jumlah;
 	float hasil;
 	float sisa;
 		scanf("%f", &inputan);
-		jumlah = inputan + data_pembelian;	
-		sisa = batas_pembelian - data_pembelian;
+		jumlah = inputan + c.purcase;	
+		sisa = c.max_purchase - c.purcase;
 
-	if( jumlah > batas_pembelian){
+	if( jumlah > c.maxpurchase){
 		printf("Anda Hanya Dapat Membeli Sebanyak : %.5f\n", sisa);
 		printf("|| Masukan Jumlah Liter : ");
 		getchar();
